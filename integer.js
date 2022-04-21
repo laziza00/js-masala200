89238741fa335a403e70045bdcf1d2b07adbd00d
laziza@mail.ru <laziza00>
2022-04-21 08:08:00 +0500

    initial commit

---
+++ integer.js
@@ -0,0 +1,69 @@
+//=============== masala 210=================
+
+// let a = 8
+// let b = 204
+
+// console.log(b.toString(a));
+//=============== masala 211=================
+
+
+// let c ="C++☺️"
+
+// function splitFunc (arr) {
+
+//     return arr.split('')
+
+// }
+// let res = splitFunc(c);
+
+// console.log(res);
+
+//=============== masala 212================
+
+
+//=============== masala 214================
+
+
+
+// let arr = [1,2,2,1]
+// let arr2 =[1,2,2,2]
+
+// let palindromeArrayFunc = (arr) => {
+  
+//      return isPalindromeFunc(arr);
+    
+// }
+// function isPalindromeFunc(arr) {
+//     let isPalindromeNumber = true
+
+//     for(let i = 0; i < arr.length / 2; i++) {
+
+//         if(arr[i] !== arr[arr.length - i - 1]){
+//             isPalindromeNumber = false; 
+//           break;
+//         }
+//     }
+    
+//     return isPalindromeNumber;
+// }
+
+// console.log(palindromeArrayFunc(arr));
+// console.log(palindromeArrayFunc(arr2));
+
+
+//=============== masala 213================
+
+
+// const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; 
+
+// function isPrime(arr) {
+//   for (let start = 2; arr > start; start++) {
+//     if (arr % start == 0) {
+//       return false;
+//     }
+//   }
+//   return arr > 1;
+// }
+
+// console.log(array.filter(isPrime)); 
+
